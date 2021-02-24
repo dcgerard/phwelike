85389357dd7c17f3b3ab2aa23ce4a38ffc623863
David Gerard <gerard.1787@gmail.com>
2021-02-24 15:38:19 -0500

    added parallelization support

--- R/multi.R
+++ R/multi.R
@@ -3,7 +3,9 @@
 #################
 
 
-#' Fit \code{\link{main_p2}()} across many SNPs
+#' Fit \code{\link{main_p2}()} across many SNPs.
+#'
+#' Support is provided for parallelization through the future package.
 #'
 #' @param nmat A matrix of counts where the columns index the genotypes and
 #'     the rows index the loci. \code{nmat[i, j]} contains the number
@@ -17,16 +19,41 @@
 #'
 #' @export
 #'
+#' @importFrom foreach %dopar%
+#' @importFrom doRNG %dorng%
+#' @import doFuture
+#' @importFrom foreach foreach
+#'
+#' @examples
+#' ## Fake data
+#' nmat <- matrix(c(1, 2, 3, 2, 1,
+#'                  3, 2, 1, 2, 3),
+#'                nrow = 2,
+#'                byrow = TRUE)
+#'
+#' ## Run the analysis in parallel on the local computer with two workers
+#' future::plan(future::multisession, workers = 2)
+#' main_multi(nmat = nmat)
+#'
+#' ## Shut down parallel workers
+#' future::plan("sequential")
+#'
 main_multi <- function(nmat) {
   stopifnot(ncol(nmat) == 5)
 
-  outmat <- matrix(NA_real_, ncol = 2, nrow = nrow(nmat))
+  ## Register doFutures() ----
+  oldDoPar <- registerDoFuture()
+  on.exit(with(oldDoPar, foreach::setDoPar(fun=fun, data=data, info=info)), add = TRUE)
+
+  ## Run foreach ----
+  nvec <- NULL
+  outmat <- foreach(nvec = iterators::iter(nmat, by = "row"),
+                   .combine = rbind) %dorng% {
+                     ## 4:0 because they code number of alternative, not reference, alleles
+                     nm <- unlist(mapply(x = 4:0, times = nvec, FUN = rep))
+                     main_p2(nm = nm)
+                   }
   colnames(outmat) <- c("r", "alpha")
-  for (i in seq_len(nrow(nmat))) {
-    nvec <- nmat[i, ]
-    ## 4:0 because they code number of alternative, not reference, alleles
-    nm <- unlist(mapply(x = 4:0, times = nvec, FUN = rep))
-    outmat[i, ] <- main_p2(nm = nm)
-  }
+
   return(data.frame(outmat))
 }
